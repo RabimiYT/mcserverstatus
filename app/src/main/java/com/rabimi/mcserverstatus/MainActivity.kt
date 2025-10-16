f313117ead35ee0c681e62e3c2835cdf1318b918
RabimiYT <fakerabimi@gmail.com>
2025-10-16 20:50:50 +0900

    Implement dark mode toggle in MainActivity
    Added theme toggle functionality to the main activity.

--- app/src/main/java/com/rabimi/mcserverstatus/MainActivity.kt
+++ app/src/main/java/com/rabimi/mcserverstatus/MainActivity.kt
@@ -2,17 +2,49 @@ package com.rabimi.mcserverstatus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rabimi.mcserverstatus.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatDelegate
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var isDarkMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.textView.text = "MCServer Status\nTPS / PING / Memory\nSurvival / Lobby / PvP"
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        updateThemeIcon(menu?.findItem(R.id.action_toggle_theme))
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_toggle_theme -> {
+                isDarkMode = !isDarkMode
+                if (isDarkMode) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    item.setIcon(R.drawable.ic_dark_mode)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    item.setIcon(R.drawable.ic_light_mode)
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateThemeIcon(item: MenuItem?) {
+        val mode = AppCompatDelegate.getDefaultNightMode()
+        if (mode == AppCompatDelegate.MODE_NIGHT_YES)
+            item?.setIcon(R.drawable.ic_dark_mode)
+        else
+            item?.setIcon(R.drawable.ic_light_mode)
     }
 }
