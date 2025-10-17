0bae084672b541305a2f002a474eb68b63272532
RabimiYT <fakerabimi@gmail.com>
2025-10-17 16:49:07 +0900

    ServerListAdapter.kt の作成

---
+++ app/src/main/java/com/rabimi/mcserverstatus/ServerListAdapter.kt
@@ -0,0 +1,35 @@
+package com.rabimi.mcserverstatus
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ServerListAdapter(private val servers: MutableList<Server>) :
+    RecyclerView.Adapter<ServerListAdapter.ServerViewHolder>() {
+
+    class ServerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.serverName)
+        val address: TextView = itemView.findViewById(R.id.serverAddress)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServerViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_server, parent, false)
+        return ServerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ServerViewHolder, position: Int) {
+        val server = servers[position]
+        holder.name.text = server.name
+        holder.address.text = server.address
+    }
+
+    override fun getItemCount() = servers.size
+
+    fun addServer(server: Server) {
+        servers.add(server)
+        notifyItemInserted(servers.size - 1)
+    }
+}
